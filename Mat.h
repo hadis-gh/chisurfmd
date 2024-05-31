a36a3b1d42c90841b011860962c3e387e84de184
Hadis Ghodrati Saeini <hadis.ghodrati-saeini@physik.tu-chemnitz.de>
2024-05-31 13:50:36 +0200

    edit file- for the meeting

--- Mat.h
+++ Mat.h
@@ -5,11 +5,11 @@
 #include "Vec.h"
 
 // using Vec<T, D> = vec;
-template<typename T, int N = 2, int M = 2>
+template<typename T, int M = 2, int N = 2>
 class Mat {
-    std::array <Vec<T, N>, M> elements;
 
-public:
+    public:
+    std::array <Vec<T, N>, M> elements;
     //constructors
     Mat() = default;
     Mat(std::array<Vec<T, N>, M> init) : elements(init) {}
@@ -35,23 +35,20 @@ public:
     template <int MM, int NN>
     Mat<Vec<T, NN>, M> operator* (const Mat<Vec<T, NN>, MM> &other) const {
         Mat<Vec<T, NN>, M> result;
-        if (N != MM){
-            std::cout << "Not allowded multiplication! " << N << " does not match " << MM << "!" << std::endl;
-            //maybe also other action?!
-        } else {
+        static_assert(N == MM, "error message/condition");
             // Initialization
-            for (int i = 0; i < M; i++) {
-                for (int j = 0; j < NN; j++) {
-                    result[i][j] = 0;
-                }
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < NN; j++) {
+                result[i][j] = 0;
             }
+        }
+        //std::fill(result.elements.begin(), result.elements.end(), Vec<T, NN>(0));
 
-            for (int i=0; i<M; i++){
-                for (int k=0; k<NN; k++){
-                    for (int j=0; j<N; j++){
-                        result[i][j] += elements[i][k] * other[k][j];
-                        //result[i,j] += elements[i,k] * elements[k,j];   //[i,j] should work but I put [i][j] just in case
-                    }
+        for (int i=0; i<M; i++){
+            for (int k=0; k<NN; k++){
+                for (int j=0; j<N; j++){
+                    result[i][j] += elements[i][k] * other[k][j];
+                    //result[i,j] += elements[i,k] * elements[k,j];   //(i, j) should work but I put [i][j] just in case
                 }
             }
         }
@@ -77,10 +74,20 @@ public:
 
         Mat<Vec<T, N>, M> inverse_mat;
         // manually calculated because we are suppose to work with 2D...
-        inverse_mat [0][0] = elements[1][1];
-        inverse_mat [0][1] = - elements[0][1];
-        inverse_mat [1][0] = - elements[1][0];
-        inverse_mat [1][1] = elements[0][0];
+        // inverse_mat [0][0] = elements[1][1];
+        // inverse_mat [0][1] = - elements[0][1];
+        // inverse_mat [1][0] = - elements[1][0];
+        // inverse_mat [1][1] = elements[0][0];
+
+        inverse_mat (0, 0) = elements[1][1];
+        inverse_mat (0, 1) = - elements[0][1];
+        inverse_mat (1, 0) = - elements[1][0];
+        inverse_mat (1, 1) = elements[0][0];
+
+        // inverse_mat (0, 0) = *this (1, 1);
+        // inverse_mat (0, 1) = - *this (0, 1);
+        // inverse_mat (1, 0) = - *this (1, 0);
+        // inverse_mat (1, 1) = *this (0, 0);
 
         for (int i=0; i<M; i++){
             for (int j=0; j<N; j++){
@@ -89,4 +96,17 @@ public:
         }
         return inverse_mat;        
     }
+};
+
+template<typename T, int M = 2, int N = 2>
+std::ostream& operator<<(std::ostream& COUT, const Mat<T, M, N>& ref){
+    COUT << "{" << std::endl;
+    for (int i=0; i<M; i++){
+        for (int j=0; j<N; j++){
+            COUT << ref(i, j) << ", ";
+        }
+        COUT << std::endl;
+    }
+    COUT << "}";
+    return COUT;    
 };
