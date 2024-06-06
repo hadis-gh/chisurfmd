c9f727096e837de5df108967b8dcc379bad2daca
Hadis Ghodrati Saeini <hadis.ghodrati-saeini@physik.tu-chemnitz.de>
2024-06-06 15:08:43 +0200

    clean the code atom_diffusion

--- atom_diffusion.cpp
+++ atom_diffusion.cpp
@@ -5,63 +5,86 @@
 #include <string>
 #include <cmath>
 #include <random>
+#include <chrono>
 #include "Vec.h"
 #include "Mat.h"
 
 template <class T>
-struct Point {
-    T x;
-    T y;
-};
+struct Point {T x,y;};
 
-const double sqrt3 = std::sqrt(3.0);
+std::vector <Vec<int, 2>> lattice_type(const std::string &name);
+void simulate_randomwalk(std::vector<unsigned int>& count, std::vector<unsigned int>& endpoint_count, const int steps, const int samples);
+void write_to_file(const std::vector<unsigned int>& count, const std::vector<unsigned int>& endpoint_count);
+std::vector <Vec<int, 2>> directions = lattice_type("square");
+const int L = 20;
 
-const Vec<int, 2> a1 ({1, 0});
-const Vec<int, 2> a2 ({0, 1});
-
-const std::array <Vec<int, 2>, 6> directions = {a1 , a2 , -a1 + a2, -a1, -a2, a1 -a2};
 
 int main() {
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<> dis(0, directions.size()-1);
-
-    const int min = 0;
-    const int L = 20;
-    std::vector<unsigned int> hist(L*L, 0); 
-    std::vector<unsigned int> hist100(L*L, 0);
+    std::vector<unsigned int> count(L*L, 0); 
+    std::vector<unsigned int> endpoint_count(L*L, 0);
 
     int steps = 100;
     int traceStep = 10;
-    int samples = 10000000;
+    int samples = 1000;
 
-    for (int a=0; a<samples; ++a) {
-        Vec<int, 2> current ({L/2, L/2});
-
-        for (int i=0; i<steps; ++i) {
-            int direction = dis(gen);
-            current += directions[direction];
-
-            if (current >=0 && current< L)
-                hist.at(current[0] + current[1]*L) += 1;
-        }
-        if (current >=0 && current< L)
-            hist100.at(current[0] + current[1]*L) += 1;
-    }
-
-    Vec<double, 2> vec1({1, 0});
-    Vec<double, 2> vec2({1/2, sqrt3/2});
-    Mat<double, 2, 2> basis({vec1, vec2});
-
-    std::ofstream outFile("atom_path.txt");
-    for (int y = min; y< min + L; ++y){
-        for (int x = min; x<min + L; ++x){
-            const int i = x-min + (y-min)*L;
-            Vec<double, 2> red_path ({double(x),double(y)});
-            const auto cart = basis * red_path;
-            outFile << cart[0] << " " << cart[1] << " " << hist[i]<< " "<< hist100[i]<< std::endl;
-        }
-    }
+    simulate_randomwalk(count, endpoint_count, steps, samples);
+    write_to_file(count, endpoint_count);
 
     return 0;
 }
+
+
+std::vector <Vec<int, 2>> lattice_type(const std::string &name){
+    std::vector <Vec<int, 2>> directions;
+    if (name=="triangular"){
+        const Vec<int, 2> a1 ({1, 0});
+        const Vec<int, 2> a2 ({0, 1});
+        directions = {a1 , a2 , -a1 + a2, -a1, -a2, a1 -a2};
+    }else if(name=="square"){
+        const Vec<int, 2> a1 ({1, 0});
+        const Vec<int, 2> a2 ({0, 1});
+        directions = {a1 , a2 , -a1, -a2};        
+    }
+    return directions;
+}
+
+void simulate_randomwalk(std::vector<unsigned int>& count, std::vector<unsigned int>& endpoint_count, const int steps, const int samples){
+    // std::random_device rd;
+    // std::mt19937 gen(rd());     //previous one
+    std::mt19937 gen(std::chrono::high_resolution_clock::now().time_since_epoch().count());
+    std::uniform_int_distribution<> distribution(0, directions.size()-1);
+
+    for (int a=0; a<samples; ++a) {
+    Vec<int, 2> current ({L/2, L/2});
+
+    for (int i=0; i<steps; ++i) {
+        int direction = distribution(gen);
+        current += directions[direction];
+
+        if (current >=0 && current< L)
+            count.at(current[0] + current[1]*L) += 1;
+    }
+    if (current >=0 && current< L)
+        endpoint_count.at(current[0] + current[1]*L) += 1;
+    }
+}
+
+void write_to_file(const std::vector<unsigned int>& count, const std::vector<unsigned int>& endpoint_count) {
+    std::ofstream output_file("atom_path.txt");
+    for (int y = 0; y < L; ++y) {
+        for (int x = 0; x < L; ++x) {
+            const int index = x + y * L;
+
+            Vec<double, 2> lattice_coords({static_cast<double>(x), static_cast<double>(y)});
+            Vec<double, 2> vec1({1, 0});
+            Vec<double, 2> vec2({1 / 2, std::sqrt(3.0) / 2});
+            Mat<double, 2, 2> basis({vec1, vec2});
+
+            const auto cartesian = basis * lattice_coords;
+
+            output_file << cartesian[0] << " " << cartesian[1] << " " 
+                        << count[index] << " " << endpoint_count[index] << std::endl;
+        }
+    }
+}
+
