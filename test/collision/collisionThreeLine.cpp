6a5ef97f0051beb625d070eeb77cb1d2d23a5a79
Hadis Ghodrati Saeini <hadis.ghodrati-saeini@physik.tu-chemnitz.de>
2024-06-21 22:43:08 +0200

    Add three line collision -first virsion

--- test/collision/collisionThreeLine.cpp
+++ test/collision/collisionThreeLine.cpp
@@ -3,7 +3,6 @@
 #include <utility>
 #include <cmath>
 #include <algorithm>
-#include <stdexcept> //runtime_error
 #include "lettuce/IntersectionLineCircle.h"
 #include "lettuce/Vec.h"
 #include "lettuce/Circle.h"
@@ -21,52 +20,67 @@ std::pair<Circle<T>, T> findClosestCircle (const Vec<T> &startPoint, const Vec<T
         } 
     }
     if (circlesDistance.empty()) {
-        throw std::runtime_error("No intersection found");
+        std::cout << "No intersection found!" << std::endl;
     }
 
     auto minDistanceIt = std::min_element(circlesDistance.begin(), circlesDistance.end());
-    T minDistance = *minDistanceIt; // Dereference iterator to get value
+    T minDistance = *minDistanceIt;
     size_t minIndex = std::distance(circlesDistance.begin(), minDistanceIt);
 
     return {circles[circlesIndex[minIndex]], minDistance};
 }
 
-template<typename T>
-Circle<T> findClosestCircle3Line (const Vec<T> &startPoint, const Vec<T> &direction, const std::vector<Circle<T>> &circles){
-    Vec<T> startPoint2 = {{startPoint[0]+direction[0], startPoint[1]-direction[1]}};
-    Vec<T> startPoint3 = {{startPoint[0]-direction[0], startPoint[1]+direction[1]}};
-    std::vector<std::pair<Circle<T>, T>> pairs = {findClosestCircle (startPoint, direction, circles), 
-                                                  findClosestCircle (startPoint2, direction, circles), 
-                                                  findClosestCircle (startPoint3, direction, circles)};
+// template<typename T>
+// Circle<T> findClosestCircle3Line (const Vec<T> &startPoint, const Vec<T> &direction, const std::vector<Circle<T>> &circles){
+//     std::vector<std::pair<Circle<T>, T>> pairs = {findClosestCircle({{startCircle.c[0] + r*dx/h, startCircle.c[0] + r*dy/h}}, direction, circles),
+//                                                   findClosestCircle({{startCircle.c[0] + r*dy/h, startCircle.c[0] - r*dx/h}}, direction, circles),
+//                                                   findClosestCircle({{startCircle.c[0] - r*dy/h, startCircle.c[0] + r*dx/h}}, direction, circles)};
 
-    auto it = std::min_element(pairs.begin(), pairs.end(),
-                              [](const std::pair<Circle<T>, T>& a,
-                                 const std::pair<Circle<T>, T>& b) {
-                                  return a.second < b.second;
-                              });
-    return it->first;
-}
+//     auto it = std::min_element(pairs.begin(), pairs.end(),
+//                               [](const std::pair<Circle<T>, T>& a,
+//                                  const std::pair<Circle<T>, T>& b) {
+//                                   return a.second < b.second;
+//                               });
+//     return it->first;
+// }
 
+float r = 1.0;
 
 int main() {
-    const Circle<float> startCircle = {{{0.0, 0.0}}, 1.0};
+    const Circle<float> startCircle = {{{-1.0, 0.0}}, r};
     const Vec<float> direction = {{1.0, 1.0}};
 
-    const std::vector<Circle<float>> circles = {{{{5.57, 5.3}}, 1.0},
-                                                {{{6.44, 5.60}}, 1.0},
-                                                {{{3.34, 3.89}}, 1.0}, 
-                                                {{{7.22, 7.50}}, 1.0}};
+    const auto h = static_cast<float>(sqrt(direction.abs2()));    
+    const auto dx = direction[0];
+    const auto dy = direction[1];
+
+    const std::vector<Circle<float>> circles = {{{{6.52, 7.26}}, r},
+                                                {{{2.00, 6.00}}, r},
+                                                {{{1.63, 3.61}}, r}, 
+                                                {{{4.59, 2.26}}, r}};
     clock_t startTime = clock();
 
-    // Circle<float> closest = findClosestCircle(startCircle.c, direction, circles);
-    Circle<float> closeCs3 = findClosestCircle3Line(startCircle.c, direction, circles);
+    // Circle<float> closeCs3 = findClosestCircle3Line(startCircle.c, direction, circles);
+    
+    std::pair<Circle<float>, float> closeTestLine1 = findClosestCircle({{startCircle.c[0] + r*dx/h, startCircle.c[0] + r*dy/h}}, direction, circles);
+    std::pair<Circle<float>, float> closeTestLine2 = findClosestCircle({{startCircle.c[0] + r*dy/h, startCircle.c[0] - r*dx/h}}, direction, circles);
+    std::pair<Circle<float>, float> closeTestLine3 = findClosestCircle({{startCircle.c[0] - r*dy/h, startCircle.c[0] + r*dx/h}}, direction, circles);
+
     clock_t endTime = clock();
     double timeTaken = double(endTime - startTime) / CLOCKS_PER_SEC;
     std::cout << "Time taken: " << timeTaken << " seconds\n";
-
     std::cout << "===========================\n";
-    // std::cout << "Closest Circle: " << closest.c << "\n";
+    // std::cout << "Closest Circle from 3 lines: Center" << closeCs3.c << "\n";
 
-    std::cout << "Closest Circle from 3 lines: Center" << closeCs3.c << "\n";
+
+
+    std::cout << "Closest Circle from 1 lines: Center" << closeTestLine1.first.c << "\n";
+    std::cout << "Closest Circle from 1 lines: " << closeTestLine1.second << "\n";
+
+    std::cout << "Closest Circle from 1 lines: Center" << closeTestLine2.first.c << "\n";
+    std::cout << "Closest Circle from 1 lines: " << closeTestLine2.second << "\n";
+
+    std::cout << "Closest Circle from 1 lines: Center" << closeTestLine3.first.c << "\n";
+    std::cout << "Closest Circle from 1 lines: " << closeTestLine3.second << "\n";
     return 0;
 }
