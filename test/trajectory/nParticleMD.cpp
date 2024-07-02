eeece0210a412b1db6962ecc7a8371846222ae5d
Hadis Ghodrati Saeini <hadis.ghodrati-saeini@physik.tu-chemnitz.de>
2024-07-02 15:16:15 +0200

    Edit MD trace trajectory, add notebook to show

--- test/trajectory/nParticleMD.cpp
+++ test/trajectory/nParticleMD.cpp
@@ -13,6 +13,7 @@
 const float sigma = 1e-10;
 const float epsilon = 1e-21;
 const float mass = 1e-25;
+const float dt = 1e-20;
 
 template<typename T>
 struct Particle{
@@ -25,7 +26,9 @@ struct Particle{
 template<typename T>
 Vec<T> calForceTwo(const Particle<T> &p1, const Particle<T> &p2){
     T r = (p2.r - p1.r).abs2();
-    return -48.0 * epsilon * (pow(sigma / r, 13) - 0.5 * pow(sigma / r, 7));
+    if (r == 0) return {{0, 0}};
+    T ljForce = 48.0 * epsilon * (pow(sigma, 12)/pow(r, 13) - 0.5 * pow(sigma,6)/pow(r, 7));
+    return ljForce * (p2.r - p1.r)/r;
 }
 
 template<typename T>
@@ -41,38 +44,83 @@ Vec<T> calTotalForce(const Particle<T> &p1, const std::vector<Particle<T>> &part
 
 template<typename T>
 Vec<T> calAccelaration(const Particle<T> &p1, const std::vector<Particle<T>> &particles){
-    return calTotalForce(p1, particles)/ mass;
+    return calTotalForce(p1, particles)/ p1.mass;
+}
+template<typename T>
+void calculateAccelerations(std::vector<Particle<T>> &particles) {
+    for (auto &p : particles) {
+        p.a = calTotalForce(p, particles) / p.mass;
+    }
+}
+
+template<typename T>         //Euler Integration
+void updateStateEuler(std::vector<Particle<T>> &particles, const T &dt){
+    for (auto &p : particles){
+        p.v += p.a * dt;
+        p.r += p.v * dt;
+        p.a = calAccelaration(p, particles);
+    }
+}
+
+template<typename T>            //Velocity Vernel Integration
+void updateStateVV(std::vector<Particle<T>> &particles, const T &dt) {
+    for (auto &p : particles) {
+        p.r += p.v * dt + p.a/2 * dt * dt;
+    }
+    std::vector<Vec<T>> old_accelerations;
+    for (const auto &p : particles) {
+        old_accelerations.push_back(p.a);
+    }
+    calculateAccelerations(particles);
+    for (size_t i = 0; i < particles.size(); ++i) {
+        particles[i].v += (old_accelerations[i] + particles[i].a)/2 * dt;
+    }
 }
 
 template<typename T>
-void updateState(Particle<T> &p1, const std::vector<Particle<T>> &particles, const T &dt){
-    p1.a = calAccelaration(p1, particles);
-    p1.v += p1.a * dt;
-    p1.r += p1.v * dt;
+void writeParticleProperties(const std::vector<Particle<T>> &particles) {
+    for (const auto &p : particles) {
+        std::cout << "Position: " << p.r << "\t"
+                  << "Velocity: " << p.v << "\t"
+                  << "Acceleration: " << p.a << std::endl;
+    }
 }
 
 template<typename T>
-void writeParticleProperties(const Particle<T> &p){
-    std::cout << "Position: \t"       << p.r << std::endl;
-    std::cout << "Velocity: \t"       << p.v << std::endl;
-    std::cout << "Acceleration: \t"   << p.a << std::endl;
+void debuggingPrintInfo(std::vector<Particle<T>> &particles, const int &numSteps, const T &dt){
+    std::cout <<"Initial states:" << std::endl;
+    writeParticleProperties(particles);
+
+    for (int step = 0; step < numSteps; ++step) {
+        updateStateVV(particles, dt);
+
+        std::cout << "\nAfter Update step " << step + 1 << ":" << std::endl;
+        writeParticleProperties(particles);
+    }
+}
+
+template<typename T>
+void writeToFile(const std::vector<Particle<T>> &particles, std::ofstream &file){
+    for (auto &p: particles){
+        file << p.r[0] << " " << p.r[1] << " ";
+    }
+    file << "\n";
 }
 
 int main(){
-    Particle<float> P1{{{0.0, 0.0}}, {{1e-5, 1e-5}}};
-    Particle<float> P2{{{5e-10, 0.0}}, {{0.0, 0.0}}};
-    Particle<float> P3{{{0.0, 5e-10}}, {{0.0, 0.0}}};
-
-    const float dt = 1e-20;
+    Particle<float> P1{{{0.0, 0.0}}, {{1e-5, 1e-5}}, {{0.0, 0.0}}, mass};
+    Particle<float> P2{{{5e-10, 0.0}}, {{0.0, 0.0}}, {{0.0, 0.0}}, mass};
+    Particle<float> P3{{{0.0, 5e-10}}, {{0.0, 0.0}}, {{0.0, 0.0}}, mass};
 
     std::vector<Particle<float>> particles {P1, P2, P3};
+    int numSteps = 10;
 
-    std::cout << "Force of P2: \t"      << calForceTwo(P1, P2) << std::endl;
+    std::ofstream file ("particlesPosMD.txt");
 
-    std::cout <<"Before Update:-----------------" << std::endl;
-    writeParticleProperties(P1);
-    std::cout <<"\nAfter Update:-----------------" << std::endl;
-    updateState(P1, particles, dt);
-    writeParticleProperties(P1);
+    for (int i = 0; i < numSteps; ++i){
+        updateStateVV(particles, dt);
+        writeToFile(particles, file);
+    }
+
     return 0;
 }
