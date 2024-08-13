1177a5de87790e7477079686b9cee548fd9e6dd0
Hadis Ghodrati Saeini <hadis.ghodrati-saeini@physik.tu-chemnitz.de>
2024-08-13 19:21:36 +0200

    edit bash script, edit visualization of Neighbors

--- bin/MDsimulation/BN_neighborTemperature.sh
+++ bin/MDsimulation/BN_neighborTemperature.sh
@@ -5,16 +5,16 @@ particlesInit="RANDOM"
 areaL=20
 particlesDensity=70.0
 time=30
-dt=0.005
+dt=0.001
 thermoInterval=0.5
 exclusionRadius=0.8
 cutoff=10.0
 seed=15
 relaxationTime=40.0
 collisionFr=$(echo "scale=4; 1 / 60" | bc -l)
-lowTemperature=0.10
+lowTemperature=0.02
 highTemperature=1.00
-stepTemperature=0.05
+stepTemperature=0.02
 saveParticles="N_Config"
 neighborFile="N_particle_NeighborsMD.dat"
 
@@ -23,29 +23,25 @@ calNeighbors() {
     local stepT="$2"
     local endT="$3"
     local fileName="$4"
-
+    prevT=$startT
     for tp in $(seq $(echo "$startT + $stepT" | bc) "$stepT" "$endT"); do
-        formattedTp=$(printf "%.2f" "$tp")
-        echo "$formattedTp" | tr '\n' '\t' >> "$fileName"
+        echo "$tp" | tr '\n' '\t' >> "$fileName"
 
-        prevT=$(echo "$tp - $stepT" | bc)
-        formattedPrevT=$(printf "%.2f" "$prevT")
-
-        testNParticleMD -T "$formattedTp" --particlesInit "${saveParticles}_${formattedPrevT}.dat" --saveParticles "${saveParticles}_${formattedTp}.dat" --appendLog --dt "$dt" -n "$particleNum" --seed "$seed" -t "$time" --areaL "$areaL" --exclusionRadius "$exclusionRadius" --thermoInterval "$thermoInterval"
+        testNParticleMD -T "$tp" --particlesInit "${saveParticles}_${prevT}.dat" --saveParticles "${saveParticles}_${tp}.dat" --appendLog --dt "$dt" -n "$particleNum" --seed "$seed" -t "$time" --areaL "$areaL" --exclusionRadius "$exclusionRadius" --thermoInterval "$thermoInterval"
 
         NearestNeighbor=$(tail -n 7 "$neighborFile" | awk '{ sum += $1 } END { if (NR > 0) print sum / NR }')
         echo "$NearestNeighbor" >> "$fileName"
 
-        echo "done for temperature: $formattedTp"
+        echo "done for temperature: $tp"
+        prevT=$tp
     done
 }
 
 > B_NeighborsCount.dat
 > B_NeighborsCount2.dat
 
-formattedStartT=$(printf "%.2f" "$highTemperature")
-testNParticleMD -T "$formattedStartT" --particlesInit "$particlesInit" --saveParticles "${saveParticles}_${formattedStartT}.dat" --dt "$dt" -n "$particleNum" --seed "$seed" -t "$time" --areaL "$areaL" --exclusionRadius "$exclusionRadius" --thermoInterval "$thermoInterval"
-echo "done for temperature: $formattedStartT"
+testNParticleMD -T "$highTemperature" --particlesInit "$particlesInit" --saveParticles "${saveParticles}_${highTemperature}.dat" --dt "$dt" -n "$particleNum" --seed "$seed" -t "$time" --areaL "$areaL" --exclusionRadius "$exclusionRadius" --thermoInterval "$thermoInterval"
+echo "done for temperature: $highTemperature"
 
 calNeighbors "$highTemperature" "-$stepTemperature" "$lowTemperature" "B_NeighborsCount.dat"
 calNeighbors "$lowTemperature" "$stepTemperature" "$highTemperature" "B_NeighborsCount2.dat"
