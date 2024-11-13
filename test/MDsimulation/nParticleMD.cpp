f6696c050f66cf771324b2da4c880a73adc0f12c
Hadis Ghodrati Saeini <hadis.ghodrati-saeini@physik.tu-chemnitz.de>
2024-11-13 16:59:23 +0100

    orgenize py for visual, wip andersen berendsen

--- test/MDsimulation/nParticleMD.cpp
+++ test/MDsimulation/nParticleMD.cpp
@@ -193,7 +193,7 @@ int main(int argc, char* argv[]) {
         const auto nextEventStep = std::min({writeStateStep, writeEnergyStep, thermoStep, nsteps});
         integrate(particles, allSpecies, dt, (nextEventStep - step) * dt, boxPBC, force, integrationMethod); //why a few times integration? 1 is not enough?
         step = nextEventStep;
-        removeCOMvelocityRotation2D(particles, allSpecies);
+        removeCOMvelocityRotation2D(particles, allSpecies); // it can be removed
 
         if (step == writeStateStep) {
             writePositionToFile(particles, positionFile, dt, step);
