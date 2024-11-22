c048d52d38173c18d09ae3052fd152db8c8fc7da
Hadis Ghodrati Saeini <hadis.ghodrati-saeini@physik.tu-chemnitz.de>
2024-11-22 20:11:23 +0100

    rename files, easy access-change para debug An WIP  for debug

--- bin/MDsimulation/template_config.sh
+++ bin/MDsimulation/template_config.sh
@@ -7,16 +7,16 @@ seed=15
 timeCooling=100
 timeHeating=100
 dt=0.001
-writeStateInterval=0.005
+writeStateInterval=0.05
 writeEnergyInterval=0.5
-thermoInterval=0.01
+thermoInterval=0.1
 
 cutoff=10.0
 exclusionRadius=0.8
 relaxationTime=40.0
 
-lowTemperature=0.05
-highTemperature=2.00
-stepTemperature=0.05
+lowTemperature=0.02
+highTemperature=1.00
+stepTemperature=0.02
 
 integration="VelocityVerlet"
