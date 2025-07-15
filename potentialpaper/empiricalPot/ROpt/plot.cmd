5da3acd4ec8ab9f90a9c6d2c4ab00a22ce6502fe
Hadis Ghodrati Saeini <hadis.ghodrati-saeini@physik.tu-chemnitz.de>
2025-07-15 12:00:04 +0200

    minor change - wip - gnuplot

--- potentialpaper/empiricalPot/ROpt/plot.cmd
+++ potentialpaper/empiricalPot/ROpt/plot.cmd
@@ -2,6 +2,6 @@
 
 plot \
 	"WW_etot_alpha_symm_RR_UU_rmin_minmax.dat" u 1:2 w l lt rgb '#800000', "" u 1:3 w l lt rgb '#800000' notitle ,\
-	"WW_etot_alpha_symm_RR_UD_rmin_minmax.dat" u 1:2 w l lt rgb '#000000', "" u 1:3 w l lt rgb '#000000' notitle ,\
-	"WW_etot_alpha_symm_RL_UU_rmin_minmax.dat" u 1:2 w l lt rgb '#000000', "" u 1:3 w l lt rgb '#000000' notitle ,\
-	"WW_etot_alpha_symm_RL_UD_rmin_minmax.dat" u 1:2 w l lt rgb '#000000', "" u 1:3 w l lt rgb '#000000' notitle ,\
+	"WW_etot_alpha_symm_RR_UD_rmin_minmax.dat" u 1:2 w l lt rgb '#8489daff', "" u 1:3 w l lt rgb '#8489daff' notitle ,\
+	"WW_etot_alpha_symm_RL_UU_rmin_minmax.dat" u 1:2 w l lt rgb '#40b588ff', "" u 1:3 w l lt rgb '#40b588ff' notitle ,\
+	"WW_etot_alpha_symm_RL_UD_rmin_minmax.dat" u 1:2 w l lt rgb '#75ed6bff', "" u 1:3 w l lt rgb '#75ed6bff' notitle ,\
