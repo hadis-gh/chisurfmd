898f4a80d444b84562a9cb7310befb9e280cf640
Hadis Ghodrati Saeini <hadis.ghodrati-saeini@physik.tu-chemnitz.de>
2026-08-03 16:00:57 +0200

    Restructure/ clean move note book- delete unused

---
+++ notebooks/config.py
@@ -0,0 +1,24 @@
+"""
+config.py
+---------
+Package-level constants.
+"""
+
+# ----------------------------------------------------------------------
+
+PLOT_PARAMS = {
+    'font.family': 'serif',      # paper
+    # 'font.family': 'Sans-serif',  # slide
+    'font.serif': 'DejaVu Serif',
+    'font.size': 11,
+    'axes.titlesize': 11,
+    'axes.labelsize': 11,
+    'xtick.labelsize': 8,
+    'ytick.labelsize': 8,
+    'figure.dpi': 150,
+    'savefig.dpi': 300,
+    'image.cmap': 'magma',
+    'text.usetex': False,
+    'mathtext.default': 'regular',
+}
+
