f9bf42797e628892f21932638aac48883e4acc69
Hadis Ghodrati Saeini <hadis.ghodrati-saeini@physik.tu-chemnitz.de>
2026-08-03 16:05:51 +0200

    Update plot_params

--- notebooks/config.py
+++ notebooks/config.py
@@ -7,18 +7,20 @@ Package-level constants.
 # ----------------------------------------------------------------------
 
 PLOT_PARAMS = {
-    'font.family': 'serif',      # paper
-    # 'font.family': 'Sans-serif',  # slide
-    'font.serif': 'DejaVu Serif',
-    'font.size': 11,
-    'axes.titlesize': 11,
-    'axes.labelsize': 11,
-    'xtick.labelsize': 8,
-    'ytick.labelsize': 8,
-    'figure.dpi': 150,
+    'font.family': 'STIXGeneral',
+    'mathtext.fontset': 'stix',
+    'mathtext.default': 'regular',
+
+    'font.size': 12,
+    'axes.titlesize': 12,
+    'axes.labelsize': 12,
+    'xtick.labelsize': 10,
+    'ytick.labelsize': 10,
+    'legend.fontsize': 10,
+
+    'figure.dpi': 200,
     'savefig.dpi': 300,
+
     'image.cmap': 'magma',
     'text.usetex': False,
-    'mathtext.default': 'regular',
 }
-
