98398da81f17e317edc8b5a646daf53d369cf710
Hadis Ghodrati Saeini <hadis.ghodrati-saeini@physik.tu-chemnitz.de>
2026-09-12 14:36:41 +0200

    fix: remove machine-specific ChiMorse model path

--- include/chisurfmd/potential/ChiMorse.h
+++ include/chisurfmd/potential/ChiMorse.h
@@ -744,8 +744,7 @@ struct ChiMorse<ParticleOriented<T>>
         desc.add_options()
             (
                 "chiMorseModel",
-                po::value<std::string>()->default_value("/home/hadis/chimorse/examples/models/chimorse_all.json"),
-                "path to ChiMorse JSON model"
+                po::value<std::string>()->required(), "path to ChiMorse JSON model"
             )
             (
                 "E0",
